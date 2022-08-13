1b59c06f69c9e9fca6b39c7b1e05217a23ce5f99
Iyliia Abramova <iuliyaabramova1974@gmail.com>
2022-08-13 18:52:38 +1100

    add task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+  let res; 
+  let D = Math.pow(b, 2) - 4 * a * c;
+  let x1, x2;
+
+  if (D > 0) {
+    x1 = ((-b + Math.sqrt(D)) / (2 * a));
+    x2 = ((-b - Math.sqrt(D)) / (2 * a));
+    return res = [x1, x2];
+
+  } else if (D == 0) {
+    x1 = x2 = (-b / (2 * a));
+    return res = [x1];
+
+  } else if (D < 0) 
+    return res = [];
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
