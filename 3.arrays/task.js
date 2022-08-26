44ed120f09eeca4ecae476c19806a269475ebce6
Iyliia Abramova <iuliyaabramova1974@gmail.com>
2022-08-27 09:15:49 +1100

    add objects task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((currentElement, index) => currentElement === arr2[index]);
+ 
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+/*function advancedFilter(arr) {
 
-  // Ваш код
+  let resultArr = arr.filter(number => number > 0).filter(number % 3 === 0).map(number => number * 10); 
 
   return resultArr; // array
+}*/
+
+function advancedFilter(arr) {
+
+  return arr.filter(number => number > 0 && number % 3 === 0).map(number => number * 10);
 }
+
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,59 @@
+'use strict';
+
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let studentOne = new Student('Ivan', 'male', 21 );
+let studentTwo = new Student('Jon', 'male', 20 );
+let studentThree = new Student('Anna', 'female', 20 );
+let studentFour = new Student('Iuliya', 'female', 22 );
+
+/*console.log(studentThree);
+console.log(studentFour);*/
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
+/*console.log(Object.getPrototypeOf(studentThree) === Student.prototype);
+console.log(studentThree.setSubject('History'));*/
 
 // ваш код для остальных методов
+
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+    return this.marks.push(mark);
+  } 
+}
+console.log(studentThree.addMark(5));
+console.log(studentThree.addMark(4));
+console.log(studentThree.addMark(5));
+
+Student.prototype.addMarks = function(mark1, mark2, mark3, ...mark) {
+  return this.marks.push(mark1, mark2, mark3, ...mark);
+}
+
+console.log(studentThree.addMarks(5, 4, 5, 5, 4));
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+}
+
+console.log(studentThree.getAverage());
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  return this.excluded = reason;
+}
+
+console.log(studentThree.exclude('low grades'));
